047d1d975d55fc04831cfefd8c6a7aa687ed791c
raghavakasibhatla <52657945+raghavakasibhatla@users.noreply.github.com>
2019-10-31 14:42:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -97,7 +97,7 @@ $dataPoints = array(
          background-color: white;
          padding: 20px;
          margin-top: 20px;
-         max-height: 465px;
+         max-height: 200px;
          overflow-y: scroll;
 
       }
