5c36c0146aaaf8a2ad6cf07d805661525f4220d8
raghavakasibhatla <52657945+raghavakasibhatla@users.noreply.github.com>
2019-11-02 14:05:16 +0530

    removed title export

--- edit.php
+++ edit.php
@@ -13,8 +13,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 <html>
 <head>
   <title>VERDENTUM</title> 
-  <link rel="icon" href="images.png" type="image" sizes="10x10">
-   <title>Export to csv</title>  
+  <link rel="icon" href="images.png" type="image" sizes="10x10">  
            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>  
            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />  
            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>  
