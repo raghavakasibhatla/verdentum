d7c06e30b9df336891070626de0f76e39cc00e4a
raghavakasibhatla <52657945+raghavakasibhatla@users.noreply.github.com>
2019-11-04 11:26:19 +0530

    data

--- datatable.php
+++ datatable.php
@@ -18,7 +18,8 @@ if ($conn->connect_error) {
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>VERDENTUM DATA</title>
+    <title>VERDENTUM</title> 
+  <link rel="icon" href="images.png" type="image" sizes="10x10">
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.10.16/datatables.min.css"/>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <meta charset="utf-8">
